ff1608138e2e73102b92cd271ae0fdd8b70b8df6
ebejmo <94139531+ebejmo@users.noreply.github.com>
2025-10-12 10:42:24 +0200

    fix: fixed naming issue. token vs accessToken

--- src/context/auth/AuthProvider.jsx
+++ src/context/auth/AuthProvider.jsx
@@ -2,7 +2,7 @@ import { useState, useMemo } from 'react';
 import { AuthContext } from './AuthContext';
 
 const USER_KEY = 'user';
-const TOKEN_KEY = 'token';
+const TOKEN_KEY = 'accessToken';
 
 const getInitialState = () => {
   const storedUser = localStorage.getItem(USER_KEY);
@@ -10,29 +10,23 @@ const getInitialState = () => {
   if (storedUser && storedToken) {
     return { user: JSON.parse(storedUser), accessToken: storedToken };
   }
-  return { user: null, token: null };
+  return { user: null, accessToken: null };
 };
 
 export function AuthProvider({ children }) {
   const [auth, setAuth] = useState(getInitialState);
 
   const login = (apiResult) => {
-    // const authState = { user: userData, accessToken: token };
-    // setAuth(authState);
-    // localStorage.setItem('user', JSON.stringify(userData));
-    // localStorage.setItem('accessToken', token);
-    const token = apiResult.accessToken;
-    const userData = apiResult;
-
-    const authState = { user: userData, token };
+    const { accessToken, ...userData } = apiResult;
+    const authState = { user: userData, accessToken };
     setAuth(authState);
 
     localStorage.setItem(USER_KEY, JSON.stringify(userData));
-    localStorage.setItem(TOKEN_KEY, token);
+    localStorage.setItem(TOKEN_KEY, accessToken);
   };
 
   const logout = () => {
-    const emptyState = { user: null, token: null };
+    const emptyState = { user: null, accessToken: null };
     setAuth(emptyState);
     localStorage.removeItem(USER_KEY);
     localStorage.removeItem(TOKEN_KEY);
@@ -43,7 +37,7 @@ export function AuthProvider({ children }) {
       ...auth,
       login,
       logout,
-      isAuthenticated: !!auth.user && !!auth.token,
+      isAuthenticated: !!auth.user && !!auth.accessToken,
       isManager: !!auth.user?.venueManager,
     }),
     [auth]
