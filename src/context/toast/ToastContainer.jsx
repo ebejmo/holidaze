1952f9dd2b3a3c41d73757918ecb537dbb0fa342
ebejmo <94139531+ebejmo@users.noreply.github.com>
2025-11-11 21:39:14 +0100

    style: removed inline styles

--- src/context/toast/ToastContainer.jsx
+++ src/context/toast/ToastContainer.jsx
@@ -2,11 +2,7 @@ import { Toast } from './Toast';
 
 export function ToastContainer({ toasts, removeToast }) {
   return (
-    <div
-      className="toast-container position-fixed bottom-0 end-0 p-3"
-      // put styles into scss
-      style={{ zIndex: 1100 }}
-    >
+    <div className="toast-container position-fixed bottom-0 end-0 p-3">
       {toasts.map(({ id, message, variant }) => (
         <Toast
           key={id}
